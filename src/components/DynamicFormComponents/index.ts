cb4a2946bc5a382aef8ec1876d5df2e35366f871
SAEED.PANAHI <panahi.projects@gmail.com>
2025-03-27 10:51:20 +0330

    export registerField to be able to register custom fields

--- src/components/DynamicFormComponents/index.ts
+++ src/components/DynamicFormComponents/index.ts
@@ -1,6 +1,7 @@
 import { DynamicForm } from "./DynamicForm";
 import { FormProvider, useForm } from "./providers/FormProvider";
+import { registerField } from "./DynamicFormField";
 export * from "./types";
 
-export { FormProvider, useForm };
+export { FormProvider, useForm, registerField };
 export default DynamicForm;
